7dcd96bbd95c37def02cb3a09b2fa767a53e931f
Matthew Lewis <matt@mplewis.com>
2012-11-09 15:23:50 -0600

    cleaning up fileUtils; still needs a lotta work

--- fileUtils.py
+++ fileUtils.py
@@ -2,16 +2,14 @@ import glob
 from loadConfig import loadConfig
 
 cfg = loadConfig()
-dataDir = cfg['dataLoc']['courseDataDir']
-dataExt = cfg['dataLoc']['courseDataExt']
+defaultDataDir = cfg['dataLoc']['courseDataDir']
+defaultDataExt = cfg['dataLoc']['courseDataExt']
 
 def getMostRecentFile():
 	return getAllFiles(latestFirst = True)[0]
 
-def getAllFiles(latestFirst = True):
-	directory = dataDir
-	extension = dataExt
-	files = glob.glob(directory + '/' + '*.' + extension)
+def getAllFiles(dataDir = defaultDataDir, dataExt = defaultDataExt, latestFirst = True):
+	files = glob.glob(dataDir + '/*.' + dataExt)
 	files.sort(reverse = latestFirst)
 	return files
 
