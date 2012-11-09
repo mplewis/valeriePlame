3502eaab4256f5b8cca2a5d9afd9033b984fcabf
Matthew Lewis <matt@mplewis.com>
2012-11-09 15:26:26 -0600

    lists files to analyze

--- runDeepStats.py
+++ runDeepStats.py
@@ -1,4 +1,10 @@
 import yaml
+import fileUtils
 from loadConfig import loadConfig
 
 cfg = loadConfig()
+courseDataDir = cfg['dataLoc']['courseDataDir']
+courseDataExt = cfg['dataLoc']['courseDataExt']
+
+filesToAnalyze = fileUtils.getAllFiles(dataDir = courseDataDir, dataExt = courseDataExt, latestFirst = False)
+print filesToAnalyze
