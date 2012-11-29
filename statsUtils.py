5bec56eadef2aecddb725cecb6d44d88c712e7b1
Matthew Lewis <matt@mplewis.com>
2012-11-29 13:04:18 -0600

    getting json, stats, and subject analysis to play nice

--- statsUtils.py
+++ statsUtils.py
@@ -22,43 +22,21 @@ openClosedRawFileLoc = statsOutputDir + '/' + openClosedRawFileName
 openClosedProcessedFileName = cfg['dataLoc']['statsFiles']['openClosedData']['processed'] + '.' + statsExt
 openClosedProcessedFileLoc = statsOutputDir + '/' + openClosedProcessedFileName
 
+subjectSeatsFileLoc = statsOutputDir + '/' + 'subjectSeats.dat'
+
+subjListFileLoc = cfg['dataLoc']['subjList']
+
 sanityCheckColumn = cfg['stats']['sanityCheckColumn']
 sanityPercent = cfg['stats']['sanityCheckPercent']
 
 undergradCoursesOnly = cfg['oneStop']['undergradCoursesOnly']
 maxCourseLevel = cfg['oneStop']['maxUndergradLevel']
 
-class DataAnalyzer:
-	def __init__(self, dataFileLoc):
-		self.file = dataFileLoc
-		self.dataRead = False
-		self.data = None
-	def refresh(self):
-		courses = fileUtils.unpickle(self.file)
-		self.data = getUndergradStats(courses)
-		self.dataRead = True
-	def getData(self):
-		if not self.dataRead:
-			self.refresh()
-		return self.data
-	def __repr__(self):
-		if not self.dataRead:
-			self.refresh()
-		fileTime = fileUtils.getFileNameFromPath(self.file)
-		return "Data from " + str(fileTime) + ': ' + str(self.data)
-
-def getUndergradStats(courseDict):
+def getOpenClosedStats(courseDict):
+	columns = ['numCoursesTotal', 'numCoursesAllSectionsClosed', 'numCoursesSomeSectionsOpen', 'numSectionsTotal', 'numSectionsClosed', 'numSectionsOpen', 'numSeatsTotal', 'numSeatsFilled', 'numSeatsOpen']
 	stats = {}
-	stats['numCoursesTotal'] = 0
-	stats['numCoursesAllSectionsClosed'] = 0
-	stats['numCoursesSomeSectionsOpen'] = 0
-	stats['numCoursesAllSectionsOpen'] = 0
-	stats['numSectionsTotal'] = 0
-	stats['numSectionsClosed'] = 0
-	stats['numSectionsOpen'] = 0
-	stats['numSeatsTotal'] = 0
-	stats['numSeatsFilled'] = 0
-	stats['numSeatsOpen'] = 0
+	for column in columns:
+		stats[column] = 0
 	for courseKey in courseDict:
 		course = courseDict[courseKey]
 		if undergradCoursesOnly and course.getCourseLevel() <= maxCourseLevel:
@@ -80,7 +58,7 @@ def getUndergradStats(courseDict):
 				stats['numSeatsOpen'] += section.getSeatsOpen()
 	return stats
 
-def processScrapedToRaw(printProgress = False):
+def processScrapedToOpenClosed(printProgress = False):
 	def statusOut():
 		out = str(pctComplete) + '% complete: ' + \
 			'File ' + str(numFilesProcessed + 1) + '/' + str(numFilesTotal) + \
@@ -132,41 +110,44 @@ def processScrapedToRaw(printProgress = False):
 					alignRightSpacer = alignRightSpacer[:-1]
 			if printProgress:
 				dynPrint(statusOut())
-			dRead = DataAnalyzer(fileToAnalyze)
-			dRead.refresh()
-			allData[fileTime] = dRead.getData()
+			courseDataRaw = fileUtils.unpickle(fileToAnalyze)
+			courseDataProc = getOpenClosedStats(courseDataRaw)
+			allData[fileTime] = courseDataProc
 			numFilesProcessed += 1
 		with open(openClosedRawFileLoc, 'w') as dataOut:
 			pickle.dump(allData, dataOut)
 
-def processRawToDiff():
-	# load and unpickle stats raw data dict
-	with open(openClosedRawFileLoc, 'r') as dataIn:
-		stats = pickle.load(dataIn)
-
-	# sanity check: remove outliers from the data set
-
+# sanity check: remove outliers from the data set
+def sanityCheck(openClosedData):
 	# get the median of the number of total sections from the data set and store it in saneMedian
 	sanityMedianData = []
-	for key in stats:
-		sanityMedianData.append(stats[key][sanityCheckColumn])
+	for key in openClosedData:
+		sanityMedianData.append(openClosedData[key][sanityCheckColumn])
 
 	saneMedian = mathUtils.getListMedian(sanityMedianData)
 
 	# if a key's number of total sections isn't within sanityPercent of the median value,
 	#    remove it from the list of values to write
-	for key in stats.keys():
-		sanityTestVal = stats[key][sanityCheckColumn]
+	for key in openClosedData.keys():
+		sanityTestVal = openClosedData[key][sanityCheckColumn]
 		if not mathUtils.withinPercent(sanityPercent, saneMedian, sanityTestVal):
-			del stats[key]
+			del openClosedData[key]
 
-	# sort dict keys into sortedKeys
-	sortedKeys = stats.keys()
+	# sort dict keys into sortedKeys and return
+	sortedKeys = openClosedData.keys()
 	sortedKeys.sort()
+	return sortedKeys
+
+def processOpenClosedToDiff():
+	# load and unpickle stats open/closed data dict
+	with open(openClosedRawFileLoc, 'r') as dataIn:
+		openClosed = pickle.load(dataIn)
+
+	sortedKeys = sanityCheck(openClosed)
 
 	# generate diff data for every column
 	for dateKey in sortedKeys:
-		dataItem = stats[dateKey]
+		dataItem = openClosed[dateKey]
 
 		# can't generate diff data for the first data point,
 		#     so only get diff data for every other point
@@ -174,7 +155,7 @@ def processRawToDiff():
 			currKeyIndex = sortedKeys.index(dateKey)
 			prevKeyIndex = currKeyIndex - 1
 			prevKey = sortedKeys[prevKeyIndex]
-			prevDataItem = stats[prevKey]
+			prevDataItem = openClosed[prevKey]
 			for colKey in dataItem.keys()[:]:
 				colKeyDiff = colKey + 'Diff'
 				colValDiff = dataItem[colKey] - prevDataItem[colKey]
@@ -184,10 +165,62 @@ def processRawToDiff():
 			dataItem['numSeatsOpenDelta'] = dataItem['numSeatsOpenDiff'] - dataItem['numSeatsTotalDiff']
 
 	with open(openClosedProcessedFileLoc, 'w') as processedDictOut:
-		pickle.dump(stats, processedDictOut)
+		pickle.dump(openClosed, processedDictOut)
+
+def getSubjSeatStats(subjList, subjDataRaw):
+	columns = ['numSeatsOpen', 'numSeatsTotal']
+	subjDataProc = {}
+	for subj in subjList:
+		subjDataProc[subj] = {}
+		for column in columns:
+			subjDataProc[subj][column] = 0
+	for courseKey in subjDataRaw:
+		course = subjDataRaw[courseKey]
+		courseSubj = course.getCourseSubj()
+		sectionsDict = course.getAllSections()
+		for sectionKey in sectionsDict:
+			section = sectionsDict[sectionKey]
+			subjDataProc[courseSubj]['numSeatsOpen'] += section.getSeatsOpen()
+			subjDataProc[courseSubj]['numSeatsTotal'] += section.getSeatsTotal()
+	return subjDataProc
+
+def processScrapedToSubjectSeats(printProgress = False):
+	subjDict = fileUtils.unpickle(subjListFileLoc)
+	subjList = sorted(subjDict.keys())
+	filesToAnalyze = fileUtils.getAllFiles(dataDir = courseDataDir, dataExt = courseDataExt, latestFirst = False)
+	try:
+		with open(subjectSeatsFileLoc, 'r') as existingDataFile:
+			allData = pickle.load(existingDataFile)
+	except IOError:
+		allData = {'_filesProcessed': set()}
+	numFilesProcessed = 0
+	numFilesTotal = len(filesToAnalyze)
+	pluralText = 'files'
+	if numFilesTotal == 1:
+		pluralText = 'file'
+	if numFilesTotal > 0:
+		openClosedData = fileUtils.unpickle(openClosedRawFileLoc)
+		saneFileKeys = sanityCheck(openClosedData)
+		existingFileKeys = allData['_filesProcessed']
+		unprocessedFileKeysSet = set(saneFileKeys).difference(existingFileKeys)
+		print len(unprocessedFileKeysSet), 'unprocessed files'
+		unprocessedFileKeys = sorted(list(unprocessedFileKeysSet))
+		print unprocessedFileKeys[len(unprocessedFileKeys)]
+		for fileTime in unprocessedFileKeys[len(unprocessedFileKeys)]:
+			fileLoc = courseDataDir + '/' + fileTime + '.' + courseDataExt
+			print fileLoc
+			subjDataRaw = fileUtils.unpickle(fileLoc)
+			subjDataProc = getSubjSeatStats(subjList, subjDataRaw)
+			allData[fileTime] = subjDataProc
+			allData['_filesProcessed'].add(fileTime)
+			print allData['_filesProcessed'], subjDataProc['CSCI']
+			numFilesProcessed += 1
+		with open(subjectSeatsFileLoc, 'w') as dataOut:
+			pickle.dump(allData, dataOut)
 
 if __name__ == '__main__':
-	processScrapedToRaw(printProgress = True)
-	dynPrint('Done. Processing raw data from ' + openClosedRawFileLoc + '...\n')
-	processRawToDiff()
-	print 'Done. Raw data processed into ' + openClosedProcessedFileLoc + '.'
+	# processScrapedToOpenClosed(printProgress = True)
+	# dynPrint('Done. Processing raw data from ' + openClosedRawFileLoc + '...\n')
+	# processOpenClosedToDiff()
+	# print 'Done. Raw data processed into ' + openClosedProcessedFileLoc + '.'
+	processScrapedToSubjectSeats(printProgress = True)
--- writeJson.py
+++ writeJson.py
@@ -9,31 +9,54 @@ statsOutputDir = cfg['dataLoc']['statsDir']
 statsExt = cfg['dataLoc']['statsFiles']['statsExt']
 openClosedProcessedFileName = cfg['dataLoc']['statsFiles']['openClosedData']['processed'] + '.' + statsExt
 openClosedProcessedFileLoc = statsOutputDir + '/' + openClosedProcessedFileName
-jsonFullOutLoc = cfg['dataLoc']['stockJson']
+jsonFullOutLoc = cfg['dataLoc']['json']['seatsAllSubjs']
+jsonDividedOutLoc = cfg['dataLoc']['json']['seatsBySubj']
 firstUsefulTime = cfg['stockChart']['firstUsefulTime']
 dataColumn = cfg['stockChart']['column']
 dataMult = cfg['stockChart']['dataMult']
+subjectDataLoc = cfg['dataLoc']['subjList']
+subjectSeatsFileLoc = statsOutputDir + '/' + 'subjectSeats.dat'
 
-diffData = fileUtils.unpickle(openClosedProcessedFileLoc)
+if __name__ == '__main__':
+	diffData = fileUtils.unpickle(openClosedProcessedFileLoc)
+	subjSeatsData = fileUtils.unpickle(subjectSeatsFileLoc)
+	del subjSeatsData['_filesProcessed']
 
-jsonData = []
+	print subjSeatsData
 
-def getUsefulSortedKeys(unorderedDict):
-	sortedKeys = sorted(unorderedDict.keys())
-	def isUsefulData(key):
-		key = int(key)
-		return key > firstUsefulTime
-	usefulKeys = filter(isUsefulData, sortedKeys)
-	return usefulKeys
+	subjectDict = fileUtils.unpickle(subjectDataLoc)
+	subjects = sorted(subjectDict.keys())
 
-usefulKeys = getUsefulSortedKeys(diffData)[1:]
+	jsonFullData = []
+	jsonSubjData = []
 
-for dateKey in usefulKeys:
-	# move everything back 6 hours from GMT so the times match up to 8am on registratio days,
-	#     then move everything back a half hour so that 8am shows the registrations after it, not before it
-	jsDate = (int(dateKey) - (6 * 3600) - (3600 / 2)) * 1000
-	data = diffData[dateKey]['numSeatsOpenDelta'] * dataMult
-	jsonData.append([jsDate, data])
+	def getUsefulSortedKeys(unorderedDict):
+		sortedKeys = sorted(unorderedDict.keys())
+		def isUsefulData(key):
+			key = int(key)
+			return key > firstUsefulTime
+		usefulKeys = filter(isUsefulData, sortedKeys)
+		return usefulKeys
 
-with open(jsonFullOutLoc, 'w') as jsonOut:
-	json.dump(jsonData, jsonOut)
+	usefulFullKeys = getUsefulSortedKeys(diffData)[1:]
+
+	for dateKey in usefulFullKeys:
+		# move everything back 6 hours from GMT so the times match up to 8am on registration days,
+		#     then move everything back a half hour so that 8am shows the registrations after it, not before it
+		jsDate = (int(dateKey) - (6 * 3600) - (3600 / 2)) * 1000
+		data = diffData[dateKey]['numSeatsOpenDelta'] * dataMult
+		jsonFullData.append([jsDate, data])
+
+	usefulSubjKeys = getUsefulSortedKeys(subjSeatsData)
+
+	for dateKey in usefulSubjKeys:
+		#for subj in subjects:
+		for subj in ['CSCI']:
+			dataPoint = subjSeatsData[dateKey]
+			if not subj in dataPoint:
+				print dateKey, subj, 'not found in', dateKey
+			else:
+				print dateKey, subj, dataPoint[subj]
+
+	with open(jsonFullOutLoc, 'w') as jsonOut:
+		json.dump(jsonFullData, jsonOut)
